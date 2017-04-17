450459712b5972394a0ca5e81e8c932fd450c51a
lijian <1076629390@qq.com>
2017-04-17 20:35:07 +0800

    webpack-dev-server

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-document.write("hello webpack");
+document.write("hello webpack666");
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,12 @@ module.exports = {
   entry: {app: './index.js'},
   output: {
    filename: 'bundle.js',
-   path: __dirname + '/dist'
+   path: __dirname + '/build'
+ },
+ devServer: {
+   port:8080,
+   contentBase: "./build",//本地服务器所加载的页面所在的目录
+  //  colors: true,//终端中输出结果为彩色
+   historyApiFallback:true,//不跳转，用于开发单页面应用，依赖于HTML5 history API 设置为true点击链接还是指向index.html
  }
 }
