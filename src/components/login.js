562e293838f8583c1658020768ab2239c1484379
lijian <1076629390@qq.com>
2017-05-07 22:30:52 +0800

    打包less和图片

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import {fetchAsync} from "../util/fetch_async.js";
+import btn_login from "../assets/img/login/btn_login.png";
+import Sing from "../assets/img/login/Sing.png";
+import bg from "../assets/img/login/imgbg.png";
+import  "../assets/less/login.less";
 
 export default class Login extends React.Component{
   constructor(){
@@ -19,14 +23,34 @@ export default class Login extends React.Component{
   }
 
   render(){
-    return (
-      <div>
-          账号：<input  /><br />
-          密码：<input type='password'/>
-          <br />
-          <button onClick={this.login}>Login()</button>
-      </div>
+    const contentStyle = {
+      overflow: 'auto'
+    }
 
+    const leftStyle={
+      margin: '0 auto',
+      float: 'left',
+    };
+
+    const rightStyle = {
+        float: 'left'
+    }
+
+    return (
+      <div className="body" style={{background:'#95c124',margin:'0 auto'}}>
+        <div className="content" style={contentStyle}>
+          <div className="left" style={leftStyle}>
+            <img src={bg}></img>
+          </div>
+          <div className='right' style={rightStyle}>
+            账号：<input  /><br />
+            密码：<input type='password'/>
+            <br />
+            <button onClick={this.login}>Login()</button>
+            <img src={btn_login} alt='登录按钮'></img>
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,45 @@
 var path = require("path");
-
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 /*
  *COMMAND JS的导出语法
  */
 module.exports = {
-  devtool: 'eval-source-map',
-  entry: {app: './index.js'},
-  output: {
-   filename: 'bundle.js',
-   path: __dirname + '/build'
- },
- module:{
-   rules: [
-      {
-        test: /\.jsx?$/,
-        use: [
-          'babel-loader',
-        ],
-        exclude: /node_modules/
-      },
-    ],
- },
- devServer: {
-   port:8080,
-   contentBase: "./build",//本地服务器所加载的页面所在的目录
-  //  colors: true,//终端中输出结果为彩色
-   historyApiFallback:true,//不跳转，用于开发单页面应用，依赖于HTML5 history API 设置为true点击链接还是指向index.html
- }
+    devtool: 'eval-source-map',
+    entry: {
+        app: './index.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/build'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: ['babel-loader'],
+                exclude: /node_modules/
+            },
+            // {
+            //   test: /\.(png|jpg|gif)$/,
+            //   loaders: ["url-loader?limit=100000&name=images/[hash:12].[ext]"],
+            //   exclude: "/node_module/"
+            // },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: ['file-loader?name=images/[hash:6].[ext]'],
+                exclude: "/node_module/"
+            }, {
+                test: /\.less$/,
+                use: ["style-loader", "css-loader", "less-loader"]
+            }
+        ]
+    },
+    devServer: {
+        port: 8080,
+        contentBase: "./build", //本地服务器所加载的页面所在的目录
+        //  colors: true,//终端中输出结果为彩色
+        historyApiFallback: true, //不跳转，用于开发单页面应用，依赖于HTML5 history API 设置为true点击链接还是指向index.html
+    },
+    plugins: [// new webpack.optimize.CommonsChunkPlugin('common.js'),
+        new ExtractTextPlugin("styles.css")]
 }
